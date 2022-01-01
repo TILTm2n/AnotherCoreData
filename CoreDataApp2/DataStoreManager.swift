51c0e29deea1c64353a96f1de02aff3e1b32df2d
Eugene <evgen29031999@bk.ru>
2022-01-01 19:50:58 +0300

    change model

---
+++ CoreDataApp2/DataStoreManager.swift
@@ -0,0 +1,83 @@
+//
+//  DataStoreManager.swift
+//  CoreDataApp2
+//
+//  Created by Eugene on 20.12.2021.
+//
+
+import Foundation
+import CoreData
+
+class DataStoreManager{
+    
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+        /*
+         The persistent container for the application. This implementation
+         creates and returns a container, having loaded the store for the
+         application to it. This property is optional since there are legitimate
+         error conditions that could cause the creation of the store to fail.
+        */
+        let container = NSPersistentContainer(name: "CoreDataApp2")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                 
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    lazy var viewContext: NSManagedObjectContext = {
+        return persistentContainer.viewContext
+    }()
+    //или можно слелать так ел=сли переменная lazy!!!
+    //lazy var viewContext: NSManagedObjectContext = persistentContainer.viewContext
+
+    // MARK: - CRUD
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    func obtainMainUser() -> User{
+        let company = Company(context: viewContext)
+        company.name = "Apple"
+        
+        let user = User(context: viewContext)
+        user.name = "Eugene"
+        user.age = 22
+        user.company = company
+        
+        do {
+            try viewContext.save()
+        } catch {
+            let error = error as NSError
+            print("\(error)")
+        }
+        
+        return user
+    }
+    
+}
--- CoreDataApp2/ViewController.swift
+++ CoreDataApp2/ViewController.swift
@@ -6,17 +6,42 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    var dataStoreManager = DataStoreManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let user = dataStoreManager.obtainMainUser()
+        
+        nameLabel.text = user.name! + "" + (user.company?.name ?? "")
+        ageLabel.text = String(user.age)
+        
+        nameLabel.sizeToFit()
+        ageLabel.sizeToFit()
         
     }
     
-    
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //закрываем клавиатуру при нажатии на return
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func updateMainUser(whith name: String){
+        //чтобы достать модель из контекста необходимо использовать NSFetchRequest
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        
+        
+    }
 
 }
 
